135968d528973fdb6f607d3a366a787019f2c065
paulwalk <paul@paulwalk.net>
2024-01-22 16:41:15 +0000

    removed temp scratch command

--- cmd/root.go
+++ cmd/root.go
@@ -13,21 +13,11 @@ var log *zap.SugaredLogger
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "nonogo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "nonogo [command]",
+	Short: "Nonogram solver",
+	Long:  "A Nonogram solver written in Go. See https://github.com/paulwalk/nonogo for more information.",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -39,6 +29,5 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "", false, "--debug=true|false")
 	rootCmd.PersistentFlags().StringVarP(&puzzleFilePath, "puzzle", "", "", "--puzzle=path-to-puzzle-file")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.AddCommand(scratchCmd)
 	rootCmd.AddCommand(solveCmd)
 }
